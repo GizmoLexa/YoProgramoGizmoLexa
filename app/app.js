746c05771ce8da8913f3d59b9d48e5ffc1d91184
GizmoLexa <guidohid59@gmail.com>
2022-02-13 00:05:19 -0300

    Footer (Contacto)

--- app/app.js
+++ app/app.js
@@ -1,12 +1,16 @@
 "use strict";
-let message = 'Hello World';
-console.log(message);
-let saludo = 'Hola pueblo';
-console.log(saludo);
-var color;
-(function (color) {
-    color[color["Verde"] = 0] = "Verde";
-    color[color["Blanco"] = 1] = "Blanco";
-})(color || (color = {}));
-let colorAuto = color.Blanco;
-console.log(colorAuto);
+class Persona {
+    constructor(nombre_persona, apellido_persona) {
+        this.apellido = apellido_persona;
+        this.nombre = nombre_persona;
+    }
+    obtenerApellido() {
+        return this.apellido;
+    }
+    static estatura() {
+        return 170;
+    }
+}
+let niña = new Persona("Monse", "Insfran");
+console.log(niña.obtenerApellido(), niña.nombre);
+console.log("Altura de la persona " + Persona.estatura());
--- app/app.ts
+++ app/app.ts
@@ -1,13 +1,21 @@
-let message: string='Hello World';
-console.log(message);
-
-let saludo: string='Hola pueblo';
-console.log(saludo);
-
-enum color {
-    Verde,
-    Blanco
-}
-let colorAuto: color=color.Blanco;
-
-console.log(colorAuto);
+class Persona {
+    nombre:string;
+    private apellido:string;
+  
+    constructor (nombre_persona:string, apellido_persona:string) {
+      this.apellido = apellido_persona;
+      this.nombre = nombre_persona;
+    }
+    obtenerApellido():string{
+      return this.apellido;
+    }
+    static estatura():number{
+      return 170;
+    }
+  }
+  
+  let niña = new Persona("Monse", "Insfran");
+  
+  console.log(niña.obtenerApellido(), niña.nombre);
+  
+  console.log("Altura de la persona "+Persona.estatura());
